cb12ba69bb5d37bd5828688ba6d81753de4b2135
zizifn <515868058@qq.com>
2023-01-10 15:46:42 +0800

    try for Controller already close

--- apps/deno-vless/src/main2.ts
+++ apps/deno-vless/src/main2.ts
@@ -1,19 +1,14 @@
 import { serve, serveTls } from 'https://deno.land/std@0.170.0/http/server.ts';
-import * as uuid from 'https://jspm.dev/uuid';
-import * as lodash from 'https://jspm.dev/lodash-es';
-import { serveClient } from './deno/client.ts';
-import { processSocket } from '../../../libs/vless-js/src/lib/vless-js.ts';
-
-const userID = Deno.env.get('UUID') || '';
-let isVaildUser = uuid.validate(userID);
-if (!isVaildUser) {
-  console.log('not set valid UUID');
-}
 
 const handler = async (req: Request): Promise<Response> => {
   const upgrade = req.headers.get('upgrade') || '';
   if (upgrade.toLowerCase() != 'websocket') {
-    return await serveClient(req, userID);
+    return new Response(`200 hello`, {
+      status: 200,
+      headers: {
+        'content-type': 'text/html; charset=utf-8',
+      },
+    });
   }
   const { socket, response } = Deno.upgradeWebSocket(req, {});
   socket.addEventListener('open', () => {});
@@ -22,7 +17,9 @@ const handler = async (req: Request): Promise<Response> => {
     if (e.data === '20m') {
       let totoal = 0;
       let count = 0;
-      const response = await fetch('https://zizi.press:8888/20m');
+      // const response = await fetch('https://zizi.press:8888/20m');
+      const response = await fetch('https://cachefly.cachefly.net/100mb.test');
+
       const body = response.body;
       for await (let chunk of body!) {
         console.log((totoal += chunk.length));
@@ -38,13 +35,6 @@ const handler = async (req: Request): Promise<Response> => {
   return response;
 };
 
-globalThis.addEventListener('beforeunload', (e) => {
-  console.log('About to exit...');
-});
-
-globalThis.addEventListener('unload', (e) => {
-  console.log('Exiting');
-});
 serve(handler, { port: 8081, hostname: '0.0.0.0' });
 
 // serveTls(handler, {
