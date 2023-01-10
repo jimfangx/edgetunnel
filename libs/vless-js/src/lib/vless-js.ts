1da0bcf3a685ea2615a4834fba851a2234d6040c
zizifn <515868058@qq.com>
2023-01-10 14:42:16 +0800

    try for Controller already close

--- libs/vless-js/src/lib/vless-js.ts
+++ libs/vless-js/src/lib/vless-js.ts
@@ -211,7 +211,7 @@ export async function processSocket({
                   // https://github.com/zizifn/edgetunnel/issues/87, hack for this issue, maybe websocket sent too many small chunk,
                   // casue v2ray client can't process.
                   chunkTimeCount++;
-                  if (chunkTimeCount < 3) {
+                  if (chunkTimeCount < 20) {
                     socket.send(chunk);
                   } else if (false) {
                     console.error(`[${address}:${port}] delay`);
@@ -220,7 +220,7 @@ export async function processSocket({
                     datas = [];
                   } else {
                     console.log(`[${address}:${port}] delay--`);
-                    await delay(100);
+                    await delay(70);
                     socket.send(chunk);
                     // datas.push(chunk);
                   }
