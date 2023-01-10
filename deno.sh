147b620d466ab184853ecad199ec093c9f05f223
zizifn <515868058@qq.com>
2023-01-10 14:51:57 +0800

    try for Controller already close

--- deno.sh
+++ deno.sh
@@ -1,1 +1,1 @@
-deno run --allow-net --allow-read --allow-write --allow-env --watch apps/deno-vless/src/main.ts
+deno run --allow-net --allow-read --allow-write --allow-env --watch apps/deno-vless/src/main2.ts
